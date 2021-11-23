dd9472bb89ea6364fd46bd4c7abe71ca08b504b9
Denis Paul <paul_denis100@yahoo.com>
2021-11-23 20:27:05 +0200

    added delete functionality for tracked stocks and refactoring

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,15 +1,20 @@
 class UserStocksController < ApplicationController
     def create
         stock = Stock.check_stock_in_db(params[:ticker])
-       
         if stock.blank?
             stock = Stock.new_lookup(params[:ticker])
             stock.save
         end
-
         @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio."
+        redirect_to my_portfolio_path
+    end
 
-        flash[:notice] = "Stock #{stock.name} was successfully added to your portofolio."
-        redirect_to my_portofolio_path
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+        redirect_to my_portfolio_path
     end
 end
